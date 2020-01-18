3e4d46da47e4d6ed32e9cbef762b33daba0ef796
Darya Plotnytska <56658938+ryaplots@users.noreply.github.com>
2020-01-18 17:09:05 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+function scrollToTop() {
+    var position =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+        scrollAnimation = setTimeout("scrollToTop()", 30);
+    } else clearTimeout(scrollAnimation);
+}
+
+window.addEventListener("scroll", function() { showFunction() });
+
+function showFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("toptexts2").style.display = "block";
+    } else {
+        document.getElementById("toptexts2").style.display = "none";
+    }
+}
