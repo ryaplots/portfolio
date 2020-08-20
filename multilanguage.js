0b7ccd81ded72abfb982d089d63a70100d70b763
ryaplots <56658938+ryaplots@users.noreply.github.com>
2020-08-20 18:51:27 +0100

    language changer

---
+++ multilanguage.js
@@ -0,0 +1,42 @@
+var languages = {
+    'en': {
+        text: "Hello"
+    },
+    'de': {
+        text: "Hallo"
+    },
+    'es': {
+        text: "Hola"
+    },
+    'fr': {
+        text: "Salut"
+    },
+    'nl': {
+        text: "Hallo"
+    },
+    'it': {
+        text: "Ciao"
+    },
+    'pl': {
+        text: "Dzień Dobry"
+    },
+    'pt': {
+        text: "Olá"
+    },
+    'ru': {
+        text: "Привет"
+    },
+    'ua': {
+        text: "Вітаю!"
+    }
+}
+
+var navlang = navigator.language;
+var langstr = navlang.substring(0, 2);
+var lang = languages[navlang];
+
+if (langstr in languages) {
+    document.getElementById('greeting').innerHTML = languages[langstr]['text'];
+} else {
+    document.getElementById('greeting').innerHTML = languages['en']['text'];
+}
